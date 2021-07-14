92fa9cfd01af39f856e837d15d383577edb33be8
Ubuntu <ubuntu@ip-172-31-220-151.ap-southeast-1.compute.internal>
2021-07-14 07:59:37 +0000

    wip

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 variable "min_size" {
   type        = number
   description = "The minimum size of the autoscale group"
-  default     = 2
+  default     = 3
 }
 
 variable "max_size" {
